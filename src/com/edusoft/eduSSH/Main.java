c8e540fa3a5bbdc1d3229784eefa18b40a2925f0
Eduardo Peña Acosta <usr1565@salesianos-lacuesta.net>
2022-02-28 12:27:52 +0000

    Feat: Added conection to the other machine

--- src/com/edusoft/eduSSH/Main.java
+++ src/com/edusoft/eduSSH/Main.java
@@ -1,8 +1,35 @@
 package com.edusoft.eduSSH;
+import com.jcraft.jsch.*;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String host = "127.0.0.1";
+        int port = 22;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Introduzca un usuario del sistema:");
+        String username = scanner.nextLine();
+
+        System.out.printf("Introduzca la contraseña del usuario:");
+        String password = scanner.nextLine();
+
+        Session session = null;
+        ChannelExec channel = null;
+
+        try{
+
+            session = new JSch().getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            System.out.println("Conexión a la máquina correcta!!");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
