4b7996e62d6b2d228a5e20987d1b3691332dca31
Eduardo Peña Acosta <usr1565@salesianos-lacuesta.net>
2022-03-01 12:48:14 +0000

    Fixed: Se cambió el bucle para solicitar otra vez el archivo al usuario en caso de existir

--- src/com/edusoft/eduSSH/Main.java
+++ src/com/edusoft/eduSSH/Main.java
@@ -1,6 +1,6 @@
 package com.edusoft.eduSSH;
-import com.jcraft.jsch.*;
 
+import com.jcraft.jsch.*;
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
@@ -26,34 +26,53 @@ public class Main {
             session = new JSch().getSession(username, host, port);
             session.setPassword(password);
             session.setConfig("StrictHostKeyChecking", "no");
+            System.out.println("Conectando con la máquina remota...");
             session.connect();
-
+            System.out.println("-------------------------------------");
             System.out.println("Conexión a la máquina correcta!!");
 
+            boolean wantExit = true;
             System.out.printf("Introduce el nombre del archivo que desea ver:");
             String file = scanner.nextLine();
 
-            channel = (ChannelExec) session.openChannel("exec");
-            channel.setCommand("cat /var/log/" + file);
 
-            ByteArrayOutputStream response = new ByteArrayOutputStream();
-            channel.setOutputStream(response);
-            channel.connect();
+            while(wantExit) {
 
-            while(channel.isConnected()) {
-                Thread.sleep(100);
+                System.out.println("Introduce 'exit' si quiere salir");
+                System.out.printf("Introduce el nombre del archivo que desea ver:");
+                file = scanner.nextLine();
+
+                switch (file) {
+                    case "exit":
+                        System.out.println("Se cerrará el programa... ¡Hasta la próxima!");
+                        wantExit = false;
+                        break;
+                    default:
+                        channel = (ChannelExec) session.openChannel("exec");
+                        channel.setCommand("cat /var/log/" + file);
+
+                        ByteArrayOutputStream response = new ByteArrayOutputStream();
+                        channel.setOutputStream(response);
+                        channel.connect();
+
+                        while(channel.isConnected()) {
+                            Thread.sleep(100);
+                        }
+
+                        String responseString = new String(response.toByteArray());
+                        if(responseString.isEmpty()) {
+                            System.out.println("El archivo que ha especificado está vacío o no existe,se termina el programa.");
+                            return;
+                        }else{
+                            System.out.println(responseString);
+                        }
+                        break;
+                }
             }
 
-            String responseString = new String(response.toByteArray());
-            if(responseString.isEmpty()) {
-                System.out.println("El archivo que ha especificado está vacío o no existe,se termina el programa.");
-                return;
-            }else{
-                System.out.println(responseString);
-            }
         }catch(JSchException | InterruptedException e){
             if(e.getMessage().equals("Auth fail")){
-                System.out.println("Se ha introducido un usuario o contraseña incorrecta");
+                System.out.println("Se ha introducido un usuario o contraseña incorrecta, reinicie la aplicación");
             }else{
                 e.printStackTrace();
             }
